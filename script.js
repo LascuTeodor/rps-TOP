25ea6c0e5bcd8feb005fafe89bf718cddd0cc5f0
Lascu Teodor <lascuteodor15@gmail.com>
2024-07-14 08:04:04 +0300

    solved getPlayerMove function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0
+let computerScore = 0
+
+
 function getRandomMove() {
     let randomMove = Math.floor(Math.random() * 3)
     switch (randomMove) {
@@ -10,7 +14,24 @@ function getRandomMove() {
         case 2:
             return 'SCISSORS'
         default:
-            console.log("Make a choice")
+            
             break;
     }
+
+    console.log(randomMove)
 }
+
+function getPlayerMove() {
+    let playerChoice = prompt("Your Move:")
+    if (playerChoice === 'ROCK') {
+        playerChoice = 'ROCK'
+    }
+    else if (playerChoice === 'PAPER') {
+        playerChoice = 'PAPER'
+    } else {
+        playerChoice = 'SCISSORS'
+    }
+    console.log(playerChoice)
+}
+
+getPlayerMove()
